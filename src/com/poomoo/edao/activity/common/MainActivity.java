8486acbad77e453d612ae72071c526a67408f5d3
lflflfmmm <454592359@qq.com>
2015-07-27 23:30:53 +0800

    at home 7.27

--- src/com/poomoo/edao/activity/common/MainActivity.java
+++ src/com/poomoo/edao/activity/common/MainActivity.java
@@ -6,18 +6,27 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.poomoo.edao.R;
+import com.poomoo.edao.adapter.Main_GridViewAdapter;
 
 public class MainActivity extends BaseActivity {
 	private TextView textView_inform, textView_ecoin, textView_goldcoin,
 			textView_point;
 	private ImageView imageView_user, imageView_position;
 	private GridView gridView;
+	private Main_GridViewAdapter gridViewAdapter;
+	private final String[] list_name = { "全国返利", "", "消费领取", "我的钱包", "交易明细",
+			"信用管理", "转账支付", "合作加盟", "爱心基金", "意道商城" };
+	private final int[] list_image = { R.drawable.ic_launcher,
+			R.drawable.ic_launcher, R.drawable.ic_launcher,
+			R.drawable.ic_launcher, R.drawable.ic_launcher,
+			R.drawable.ic_launcher, R.drawable.ic_launcher,
+			R.drawable.ic_launcher, R.drawable.ic_launcher };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO 自动生成的方法存根
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_registration);
+		setContentView(R.layout.activity_main);
 		init();
 	}
 
@@ -31,6 +40,8 @@ public class MainActivity extends BaseActivity {
 		imageView_position = (ImageView) findViewById(R.id.main_imageView_position);
 		gridView = (GridView) findViewById(R.id.main_gridVeiw);
 
-	}
+		gridViewAdapter = new Main_GridViewAdapter(this, list_name, list_image);
+//		gridView.setAdapter(gridViewAdapter);
 
+	}
 }
--- src/com/poomoo/edao/adapter/Main_GridViewAdapter.java
+++ src/com/poomoo/edao/adapter/Main_GridViewAdapter.java
@@ -1,7 +1,5 @@
 package com.poomoo.edao.adapter;
 
-import java.util.List;
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,11 +12,11 @@ import com.poomoo.edao.R;
 
 public class Main_GridViewAdapter extends BaseAdapter {
 
-	private List<String> list_name;
+	private String[] list_name;
 	private int[] list_image;
 	private LayoutInflater inflater;
 
-	public Main_GridViewAdapter(Context context, List<String> list_name,
+	public Main_GridViewAdapter(Context context, String[] list_name,
 			int[] list_image) {
 		super();
 		this.list_name = list_name;
@@ -29,13 +27,13 @@ public class Main_GridViewAdapter extends BaseAdapter {
 	@Override
 	public int getCount() {
 		// TODO 自动生成的方法存根
-		return list_name.size();
+		return list_name.length;
 	}
 
 	@Override
 	public Object getItem(int position) {
 		// TODO 自动生成的方法存根
-		return list_name.get(position);
+		return list_name[position];
 	}
 
 	@Override
@@ -47,6 +45,7 @@ public class Main_GridViewAdapter extends BaseAdapter {
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		// TODO 自动生成的方法存根
+		System.out.println("getview");
 		ViewHolder viewHolder = null;
 		if (convertView == null) {
 			convertView = inflater.inflate(R.layout.acticity_main_item, parent,
@@ -60,7 +59,7 @@ public class Main_GridViewAdapter extends BaseAdapter {
 		} else {
 			viewHolder = (ViewHolder) convertView.getTag();
 		}
-		viewHolder.textView.setText(list_name.get(position));
+		viewHolder.textView.setText(list_name[position]);
 		viewHolder.imageView.setBackgroundResource(list_image[position]);
 		return null;
 	}
