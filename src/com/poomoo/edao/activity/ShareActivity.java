c01d1bc9479df8f2b13cfb7704e3f25995d5c511
lflflfmmm <454592359@qq.com>
2015-08-10 23:36:09 +0800

    at home 8.10

--- src/com/poomoo/edao/activity/ShareActivity.java
+++ src/com/poomoo/edao/activity/ShareActivity.java
@@ -10,8 +10,19 @@ import com.poomoo.edao.R;
 import com.umeng.socialize.bean.SHARE_MEDIA;
 import com.umeng.socialize.controller.UMServiceFactory;
 import com.umeng.socialize.controller.UMSocialService;
+import com.umeng.socialize.media.QQShareContent;
+import com.umeng.socialize.media.QZoneShareContent;
+import com.umeng.socialize.media.SinaShareContent;
+import com.umeng.socialize.media.SmsShareContent;
 import com.umeng.socialize.media.UMImage;
+import com.umeng.socialize.sso.QZoneSsoHandler;
+import com.umeng.socialize.sso.SinaSsoHandler;
+import com.umeng.socialize.sso.SmsHandler;
+import com.umeng.socialize.sso.UMQQSsoHandler;
 import com.umeng.socialize.sso.UMSsoHandler;
+import com.umeng.socialize.weixin.controller.UMWXHandler;
+import com.umeng.socialize.weixin.media.CircleShareContent;
+import com.umeng.socialize.weixin.media.WeiXinShareContent;
 
 /**
  * 
@@ -35,53 +46,154 @@ public class ShareActivity extends BaseActivity implements OnClickListener {
 		// 实现沉浸式状态栏效果
 		setImmerseLayout(findViewById(R.id.navigation_fragment));
 		init();
-		umeng();
+		// 配置需要分享的相关平台
+		configPlatforms();
+		// 设置分享内容
+		shareContent();
 	}
 
 	private void init() {
 		// TODO 自动生成的方法存根
 		button_share = (Button) findViewById(R.id.share_button);
-		mController.getConfig().removePlatform(SHARE_MEDIA.RENREN,
-				SHARE_MEDIA.DOUBAN);
 		button_share.setOnClickListener(this);
 	}
 
-	public void umeng() {
-		// 设置分享内容
-		mController
-				.setShareContent("友盟社会化组件（SDK）让移动应用快速整合社交分享功能，http://www.umeng.com/social");
-		// 设置分享图片, 参数2为图片的url地址
-		mController.setShareMedia(new UMImage(this,
-				"http://www.umeng.com/images/pic/banner_module_social.png"));
-		// 设置分享图片，参数2为本地图片的资源引用
-		// mController.setShareMedia(new UMImage(getActivity(),
-		// R.drawable.icon));
-		// 设置分享图片，参数2为本地图片的路径(绝对路径)
-		// mController.setShareMedia(new UMImage(getActivity(),
-		// BitmapFactory.decodeFile("/mnt/sdcard/icon.png")));
+	/**
+	 * 配置分享平台参数</br>
+	 */
+	private void configPlatforms() {
+		// 添加新浪SSO授权
+		mController.getConfig().setSsoHandler(new SinaSsoHandler());
 
-		// 设置分享音乐
-		// UMusic uMusic = new
-		// UMusic("http://sns.whalecloud.com/test_music.mp3");
-		// uMusic.setAuthor("GuGu");
-		// uMusic.setTitle("天籁之音");
-		// 设置音乐缩略图
-		// uMusic.setThumb("http://www.umeng.com/images/pic/banner_module_social.png");
-		// mController.setShareMedia(uMusic);
+		// 添加QQ、QZone平台
+		addQQQZonePlatform();
 
-		// 设置分享视频
-		// UMVideo umVideo = new UMVideo(
-		// "http://v.youku.com/v_show/id_XNTE5ODAwMDM2.html?f=19001023");
-		// 设置视频缩略图
-		// umVideo.setThumb("http://www.umeng.com/images/pic/banner_module_social.png");
-		// umVideo.setTitle("友盟社会化分享!");
-		// mController.setShareMedia(umVideo);
+		// 添加微信、微信朋友圈平台
+		addWXPlatform();
+
+		// 添加短信平台
+		addSMS();
+	}
+
+	public void shareContent() {
+		// 本地图片
+		UMImage localImage = new UMImage(this, R.drawable.ic_2dimencode);
+
+		// 配置SSO
+		mController.getConfig().setSsoHandler(new SinaSsoHandler());
+
+		QZoneSsoHandler qZoneSsoHandler = new QZoneSsoHandler(this,
+				"100424468", "c7394704798a158208a74ab60104f0ba");
+
+		WeiXinShareContent weixinContent = new WeiXinShareContent();
+		weixinContent
+				.setShareContent("来自友盟社会化组件（SDK）让移动应用快速整合社交分享功能-微信。http://www.umeng.com/social");
+		weixinContent.setTitle("友盟社会化分享组件-微信");
+		weixinContent.setTargetUrl("http://www.umeng.com/social");
+		weixinContent.setShareMedia(localImage);
+		mController.setShareMedia(weixinContent);
+
+		// 设置朋友圈分享的内容
+		CircleShareContent circleMedia = new CircleShareContent();
+		circleMedia
+				.setShareContent("来自友盟社会化组件（SDK）让移动应用快速整合社交分享功能-朋友圈。http://www.umeng.com/social");
+		circleMedia.setTitle("友盟社会化分享组件-朋友圈");
+		circleMedia.setShareMedia(localImage);
+		circleMedia.setTargetUrl("http://www.umeng.com/social");
+
+		// 设置QQ空间分享内容
+		QZoneShareContent qzone = new QZoneShareContent();
+		qzone.setShareContent("share test");
+		qzone.setTargetUrl("http://www.umeng.com");
+		qzone.setTitle("QZone title");
+		qzone.setShareMedia(localImage);
+		mController.setShareMedia(qzone);
+
+		// 设置QQ分享内容
+		QQShareContent qqShareContent = new QQShareContent();
+		qqShareContent.setShareContent("来自友盟社会化组件（SDK）让移动应用快速整合社交分享功能 -- QQ");
+		qqShareContent.setTitle("hello, title");
+		qqShareContent.setShareMedia(localImage);
+		qqShareContent.setTargetUrl("http://www.umeng.com/social");
+		mController.setShareMedia(qqShareContent);
+
+		// 设置短信分享内容
+		SmsShareContent sms = new SmsShareContent();
+		sms.setShareContent("来自友盟社会化组件（SDK）让移动应用快速整合社交分享功能-短信。http://www.umeng.com/social");
+		sms.setShareImage(localImage);
+		mController.setShareMedia(sms);
+
+		// 设置新浪微博分享内容
+		SinaShareContent sinaContent = new SinaShareContent();
+		sinaContent
+				.setShareContent("来自友盟社会化组件（SDK）让移动应用快速整合社交分享功能-新浪微博。http://www.umeng.com/social");
+		sinaContent.setShareImage(localImage);
+		mController.setShareMedia(sinaContent);
+
+	}
+
+	/**
+	 * @功能描述 : 添加QQ平台支持 QQ分享的内容， 包含四种类型， 即单纯的文字、图片、音乐、视频. 参数说明 : title, summary,
+	 *       image url中必须至少设置一个, targetUrl必须设置,网页地址必须以"http://"开头 . title :
+	 *       要分享标题 summary : 要分享的文字概述 image url : 图片地址 [以上三个参数至少填写一个] targetUrl
+	 *       : 用户点击该分享时跳转到的目标地址 [必填] ( 若不填写则默认设置为友盟主页 )
+	 * @return
+	 */
+	private void addQQQZonePlatform() {
+		String appId = "100424468";
+		String appKey = "c7394704798a158208a74ab60104f0ba";
+		// 添加QQ支持, 并且设置QQ分享内容的target url
+		UMQQSsoHandler qqSsoHandler = new UMQQSsoHandler(this, appId, appKey);
+		qqSsoHandler.setTargetUrl("http://www.umeng.com/social");
+		qqSsoHandler.addToSocialSDK();
+
+		// 添加QZone平台
+		QZoneSsoHandler qZoneSsoHandler = new QZoneSsoHandler(this, appId,
+				appKey);
+		qZoneSsoHandler.addToSocialSDK();
+	}
+
+	/**
+	 * @功能描述 : 添加微信平台分享
+	 * @return
+	 */
+	private void addWXPlatform() {
+		// 注意：在微信授权的时候，必须传递appSecret
+		// wx967daebe835fbeac是你在微信开发平台注册应用的AppID, 这里需要替换成你注册的AppID
+		String appId = "wx967daebe835fbeac";
+		String appSecret = "5bb696d9ccd75a38c8a0bfe0675559b3";
+		// 添加微信平台
+		UMWXHandler wxHandler = new UMWXHandler(this, appId, appSecret);
+		wxHandler.addToSocialSDK();
+
+		// 支持微信朋友圈
+		UMWXHandler wxCircleHandler = new UMWXHandler(this, appId, appSecret);
+		wxCircleHandler.setToCircle(true);
+		wxCircleHandler.addToSocialSDK();
+	}
+
+	/**
+	 * 添加短信平台</br>
+	 */
+	private void addSMS() {
+		// 添加短信
+		SmsHandler smsHandler = new SmsHandler();
+		smsHandler.addToSocialSDK();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		mController.getConfig().cleanListeners();
 	}
 
 	@Override
 	public void onClick(View v) {
 		// TODO 自动生成的方法存根
 		// 是否只有已登录用户才能打开分享选择页
+		mController.getConfig().setPlatforms(SHARE_MEDIA.WEIXIN,
+				SHARE_MEDIA.WEIXIN_CIRCLE, SHARE_MEDIA.QQ, SHARE_MEDIA.QZONE,
+				SHARE_MEDIA.SINA, SHARE_MEDIA.TENCENT);
 		mController.openShare(this, false);
 	}
 
