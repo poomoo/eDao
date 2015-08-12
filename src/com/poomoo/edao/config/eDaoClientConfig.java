e32fe121a563a3a5d7731b32cafd10b48f70cc25
poomoo <454592359@qq.com>
2015-08-12 10:06:50 +0800

    at co 8.12

---
+++ src/com/poomoo/edao/config/eDaoClientConfig.java
@@ -0,0 +1,13 @@
+package com.poomoo.edao.config;
+
+public class eDaoClientConfig {
+	/**
+	 * 网络相关
+	 */
+
+	// IP
+	public static final String url = "http://192.168.0.112:8080/yidao/app/call.htm";// 本地
+	// public static final String url =
+	// "";//远程
+
+}
