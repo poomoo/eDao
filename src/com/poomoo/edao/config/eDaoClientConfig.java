b5414f8797799a1973ec348ac1e72d5621441305
poomoo <454592359@qq.com>
2015-09-09 18:02:12 +0800

    at co 9.9

--- src/com/poomoo/edao/config/eDaoClientConfig.java
+++ src/com/poomoo/edao/config/eDaoClientConfig.java
@@ -6,13 +6,14 @@ public class eDaoClientConfig {
 	 */
 
 	// IP http://www.yidao7.com/app/orders/wxPayCallBack.json
-	public static final String BaseUrl = "http://192.168.0.112:8080/yidao/app/";
-	public static final String url = BaseUrl + "call.htm";// 本地
+	public static final String BaseLocalUrl = "http://192.168.0.112:8080/yidao/app/";
+	public static final String BaseRemoteUrl = "http://www.yidao7.com/yidao/app/";
+	public static final String url = BaseRemoteUrl + "call.htm";// 本地
 	// public static final String wxReUrl = BaseUrl +
 	// "orders/wxPayCallBack.json";
 	public static final String wxReUrl = "http://www.yidao7.com/app/orders/wxPayCallBack.json";
-	public static final String imageurl = BaseUrl + "user/uploadUserImage.json";
-	public static final String uploadStoreurl = BaseUrl + "shop/add.json";
+	public static final String imageurl = BaseRemoteUrl + "user/uploadUserImage.json";
+	public static final String uploadStoreurl = BaseRemoteUrl + "shop/add.json";
 
 	public static final int timeout = 1 * 30 * 1000;// 网络通讯超时
 	public static final int advTime = 1 * 5 * 1000;// 广告轮播时间
