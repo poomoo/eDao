3b3be9b43b5ebba7ab85aeeda38808ed4a6c708d
poomoo <454592359@qq.com>
2015-09-25 17:42:21 +0800

    at co 9.25

--- src/com/poomoo/edao/activity/RepayManageActivity.java
+++ src/com/poomoo/edao/activity/RepayManageActivity.java
@@ -1,12 +1,31 @@
 package com.poomoo.edao.activity;
 
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.gson.Gson;
 import com.poomoo.edao.R;
 import com.poomoo.edao.application.eDaoClientApplication;
+import com.poomoo.edao.config.eDaoClientConfig;
+import com.poomoo.edao.model.OrderListData;
+import com.poomoo.edao.model.RepayData;
+import com.poomoo.edao.model.ResponseData;
+import com.poomoo.edao.util.HttpCallbackListener;
+import com.poomoo.edao.util.HttpUtil;
 import com.poomoo.edao.util.Utity;
 
+import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.DatePicker;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -21,6 +40,10 @@ public class RepayManageActivity extends BaseActivity implements OnClickListener
 	private TextView textView_username, textView_phonenum, textView_year, textView_month, textView_day,
 			textView_year_money, textView_month_money, textView_day_money;
 	private eDaoClientApplication application = null;
+
+	private Calendar cal = Calendar.getInstance();
+	private Gson gson = new Gson();
+	private String curDate = "";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -50,25 +73,100 @@ public class RepayManageActivity extends BaseActivity implements OnClickListener
 		textView_day.setOnClickListener(this);
 
 		Utity.setUserAndTel(textView_username, textView_phonenum, application);
+
+		textView_year.setText(cal.get(Calendar.YEAR) + "年");
+		textView_month.setText((cal.get(Calendar.MONTH) + 1) + "月");
+		textView_day.setText(cal.get(Calendar.DAY_OF_MONTH) + "日");
+
+		curDate = cal.get(Calendar.YEAR) + "-" + (cal.get(Calendar.MONTH) + 1) + "-" + cal.get(Calendar.DAY_OF_MONTH);
+		getData(curDate);
 	}
 
 	@Override
 	public void onClick(View v) {
 		// TODO 自动生成的方法存根
-		switch (v.getId()) {
-		case R.id.key_and_operate_layout_key_manage:
-			openActivity(KeyManageActivity.class);
-			finish();
-			break;
-		case R.id.key_and_operate_layout_operate_manage:
-			openActivity(OperateManageActivity.class);
-			finish();
-			break;
-		case R.id.key_and_operate_layout_repay_manage:
-			openActivity(OperateManageActivity.class);
-			finish();
-			break;
-		}
+		pickDate();
 	}
 
+	private void pickDate() {
+		final DatePickerDialog mDialog = new DatePickerDialog(this, null, cal.get(Calendar.YEAR),
+				cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
+
+		// 手动设置按钮
+		mDialog.setButton(DialogInterface.BUTTON_POSITIVE, "完成", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// 通过mDialog.getDatePicker()获得dialog上的DatePicker组件，然后可以获取日期信息
+				DatePicker datePicker = mDialog.getDatePicker();
+				int year = datePicker.getYear();
+				int month = datePicker.getMonth() + 1;
+				int day = datePicker.getDayOfMonth();
+
+				textView_year.setText(year + "年");
+				textView_month.setText(month + "月");
+				textView_day.setText(day + "日");
+
+				curDate = year + "-" + month + "-" + day;
+				getData(curDate);
+			}
+		});
+		// 取消按钮，如果不需要直接不设置即可
+		mDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "取消", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+			}
+		});
+
+		mDialog.show();
+	}
+
+	private void getData(String date) {
+		// TODO 自动生成的方法存根
+		showProgressDialog("请稍后...");
+		Map<String, Object> data = new HashMap<String, Object>();
+		data.put("bizName", "70000");
+		data.put("method", "70011");
+		data.put("userId", application.getUserId());
+		data.put("date", date);
+
+		HttpUtil.SendPostRequest(gson.toJson(data), eDaoClientConfig.url, new HttpCallbackListener() {
+
+			@Override
+			public void onFinish(final ResponseData responseData) {
+				// TODO 自动生成的方法存根
+				runOnUiThread(new Runnable() {
+
+					@Override
+					public void run() {
+						// TODO 自动生成的方法存根
+						closeProgressDialog();
+						if (responseData.getRsCode() == 1) {
+							RepayData data = new RepayData();
+							data = gson.fromJson(responseData.getJsonData(), RepayData.class);
+							textView_year_money.setText("￥" + data.getYearIncome());
+							textView_month_money.setText("￥" + data.getMonthIncome());
+							textView_day_money.setText("￥" + data.getDayIncome());
+						} else {
+							Utity.showToast(getApplicationContext(), responseData.getMsg());
+						}
+					}
+
+				});
+			}
+
+			@Override
+			public void onError(Exception e) {
+				// TODO 自动生成的方法存根
+
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						// TODO 自动生成的方法存根
+						closeProgressDialog();
+						Utity.showToast(getApplicationContext(), eDaoClientConfig.checkNet);
+					}
+				});
+			}
+		});
+	}
 }
---
+++ src/com/poomoo/edao/model/RepayData.java
@@ -0,0 +1,39 @@
+package com.poomoo.edao.model;
+
+/**
+ * 
+ * @ClassName RepayData
+ * @Description TODO 报酬
+ * @author 李苜菲
+ * @date 2015年9月25日 下午5:13:34
+ */
+public class RepayData {
+	private String yearIncome = "";
+	private String monthIncome = "";
+	private String dayIncome = "";
+
+	public String getYearIncome() {
+		return yearIncome;
+	}
+
+	public void setYearIncome(String yearIncome) {
+		this.yearIncome = yearIncome;
+	}
+
+	public String getMonthIncome() {
+		return monthIncome;
+	}
+
+	public void setMonthIncome(String monthIncome) {
+		this.monthIncome = monthIncome;
+	}
+
+	public String getDayIncome() {
+		return dayIncome;
+	}
+
+	public void setDayIncome(String dayIncome) {
+		this.dayIncome = dayIncome;
+	}
+
+}
